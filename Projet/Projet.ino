61b6886ff8bfe197489c6509150300393f7c8910
TanguyBoulard <boulard@etud.insa-toulouse.fr>
2021-03-19 15:31:26 +0100

    Version 1.3

--- Projet/Projet.ino
+++ Projet/Projet.ino
@@ -1,4 +1,4 @@
-//=============================Module Bluetooth=====================
+//=============================Bluetooth Module=====================
 #include <SoftwareSerial.h>
 
 #define rxPin 13
@@ -6,168 +6,175 @@
 #define baudrate 9600
 SoftwareSerial mySerial(rxPin,txPin);
 
-//=============================RotoEncodeur=========================
-#define encoderPinA  2  //le signal d'horloge du rotoEncodeur doit être calbé sur le pin 2 pour y mettre l'interrupt
-#define encoderPinB  10  //DT du rotoEncodeur
-#define Switch 11 // SW du rotoEncodeur
+//=============================Roto Encoder=========================
+#define encoderPinA  2  //The clock signal of the Roto Encoder must be on the pin 2 since we want to use an attachInterrupt on it 
+#define encoderPinB  10  //DT signal of the Roto Encoder
+#define Switch 11 //SW signal of the Roto Encoder
 
-int aState; //état du pin A du rotoEncodeur
-int aLastState;  //état précedent du pin A du rotoEncodeur
-int btnState = HIGH; //état du bouton
-int btnLastState = HIGH;
-//=============================écran Oled===========================
+int aState; //State of the pin A of the Roto Encoder
+int aLastState;  //Last state of the pin A of the Roto Encoder
+int btnState = HIGH; //State of the button 
+int btnLastState = HIGH; //Last state of the button
+
+//=============================OLED Screen==========================
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define SCREEN_WIDTH 128 //OLED display width, in pixels
+#define SCREEN_HEIGHT 32 //OLED display height, in pixels
 
-
-// On an arduino UNO:       A4(SDA), A5(SCL)
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define OLED_RESET -1 //Reset pin shared with the arduino reset button
+#define SCREEN_ADDRESS 0x3C //Adress of the OLED Screen
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+//=============================Measure==============================
+#define pinAnalog A0 //Pin used to measure the Sensor
 
-//=============================Mesure===============================
-#define pinAnalog A0
-float mesure = 0.0;
-float valeur = 0.0;
-float resistance = 0.0;
-float moyenne[10];
-float moy = 0.0;
-float R0 = 1000; 
+//Var used to measure
+float mesure = 0.0; //Deflection
+float valeur = 0.0; //Tension (0 = 0V; 1024 = 5V)
+float resistance = 0.0; //Resistance of the Sensor
+float moyenne[10]; //Last ten values of the tension
+float moy = 0.0; //Mean value of the tension
+float R0 = 0.0; //Resistance at rest of the Sensor
 
-float sensi = 8.55;
-float offset = -25.1;
-int etalonnage2Points = 0;
-float courbure = 0;
-float mesurePoint1;
-float mesurePoint2;
-float courburePoint1;
-float courburePoint2;
+//we suppose that deflection = Slope * DeltaR + Offset
+float sensi = 8.55; //Slope of the Sensor 
+float offset = -25.1; //Offset of the Sensor
+
+//Var used for the two points calibration
+int etalonnage2Points = 0; 
+float courbure = 0.0;
+float mesurePoint1 = 0.0;
+float mesurePoint2 = 0.0;
+float courburePoint1 = 0.0;
+float courburePoint2 = 0.0;
+
 //=============================Timer================================
-int btnT0 = millis();
-int btnTimer = millis();
-
 int rotoT0 = millis();
 int rotoTimer = millis();
 
 int T0 = millis();
 int Timer = millis();
-//=============================Menu=================================
-int mode = 1;
-int selection = 1;
-int varSelection = 0;
 
-/*Fonction Setup appelée une fois au démarrage*/
+//=============================Menu=================================
+int mode = 1; //Var that tells in which menu you are
+int selection = 1; //Var used to select which menu you want to go to
+
+//=============================Setup================================
+/*Function used at the start of the Arduino*/
 void setup(){
-  //Initialisation du module Bluetooth
+  //Initialization of the Bluetooth module
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
   mySerial.begin(baudrate);
   
-  //Initialisation du rotoEncodeur
+  //Initialization of the Roto Encoder
   pinMode(encoderPinA, INPUT);
   digitalWrite(encoderPinA, HIGH);
   pinMode(encoderPinB, INPUT);
   digitalWrite(encoderPinB, HIGH);
-  attachInterrupt(digitalPinToInterrupt(2), doEncoder, RISING); //On attache l'exécution de la fonction "doEncoder" lors de la détection d'un front montant sur le pin digital 2
+  attachInterrupt(digitalPinToInterrupt(2), doEncoder, RISING);
   aLastState = digitalRead(encoderPinA);
 
   pinMode(Switch, INPUT_PULLUP);
 
-  //Initialisation de l'écran Oled
+  //Initialization of the OLED Screen
   display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
   display.display();
   delay(2000);
-  Introduction();
+  
+  for(int i = 0; i < 60; i++){
+    Introduction(i);
+  }
   delay(2000);
   
-  // Clear the buffer
-  display.clearDisplay();
-  
   Serial.begin(baudrate);
+
+  //We measure the resistance at rest of the Sensor
   valeur = (float)(analogRead(pinAnalog) * (float)(5.0/1024.0));
   R0 = (float)((1.0+100000.0/1000.0)*100000.0*(5.0/valeur)-100000.0-10000.0)/1000000.0 ;
 }
 
-
-/*Fonction loop appelée en continue*/
+//=============================Main Loop============================
+/*Function used every X ms*/
 void loop() {
-  //On met à jour le timer
-  btnTimer = millis();
+  //Updating timers
   rotoTimer = millis();
 
-  int i = 0;
-  char textSerial[32] = {0};
-  char textBluetooth[32] = {0};
-
+  //We measure the tension on the pin A0
   valeur = (float)(analogRead(pinAnalog) * (float)(5.0/1024.0));
-  
+
+  //We store the value in a table which contains the last ten values of the tension
   for(int j = 0; j < 9;  j++){
     moyenne[j] = moyenne[j+1];
   }
+  
+  //We calculate the average value of this table
   moyenne[9] = valeur;
   moy = 0.0;
   for(int j = 0; j < 10;  j++){
     moy += moyenne[j];
   }
   moy = (float)moy/10.0;
-  
+
+  //We calculate the value of the resistance
   if(moy != 0){
     resistance = (float)((1.0+100000.0/1000.0)*100000.0*(5.0/moy)-100000.0-10000.0)/1000000.0 ;
   }else{
     resistance = -1;
   }
   mesure = (float)((float)((resistance-R0)/R0)*100.0 * sensi) + offset;
-  
-  Serial.println(resistance);
-  
-  /*Du module bluetooth vers la carte arduino*/
-  //Tant que la communication entre la carte et le module bluetooth est ouverte 
-  while (mySerial.available()){
-    //On vient afficher sur le port série (carte vers ordinateur) ce qu'on lit sur le module bluetooth
+
+  //We send the value to the computer
+  if(Serial.available()){Serial.println(resistance);}
+
+  int i = 0;
+  char textBluetooth[32] = {0};
+  /*Communication from the bluetooth module to the arduino*/
+  while (mySerial.available()){ //While the com is open
     i = 0;
-    do{
+    do{ //We read what the bluetooth module send to the arduino
       textBluetooth[i++] = mySerial.read();
       delay(3);
     }while(mySerial.available());
-    //Serial.println(textBluetooth);
+    //if the message read is "mesure" it means the phone is waiting for the resistance's value
     String test = (String)textBluetooth;
     test.trim();
     if(test == "mesure"){
+      //We send the resistance's value to the bluetooth module
       mySerial.print(resistance);
     }
   }
-  
+
+  //We uptdate the button state
   btnLastState = btnState;
   btnState = digitalRead(Switch);
-  if(btnState == LOW and btnLastState != LOW){
-    if(btnTimer - btnT0 > 500){
-      /*Chose a faire quand le bouton est enfoncé*/
-      btnT0 = millis();
-    }
-  }
-   
+
+  /*Switch that draws on the screen the right menu*/
   display.clearDisplay();
+  display.drawRect(1, 1, 126,31, WHITE);
   switch (mode){
     case 1:
+      //Measurement menu
       MenuMesure();
       if(btnState == LOW and btnLastState != LOW){mode = 5;}
       break;
     case 2:
+      //Calibration menu
       MenuEtalonnage();
       if(btnState == LOW and btnLastState != LOW){mode = 5;}
       break;
     case 3:
+      //2 points calibration menu
       MenuEtalonnage2Points();
       if(btnState == LOW and btnLastState != LOW){
         etalonnage2Points += 1;
         if(etalonnage2Points == 3){
           mode = 5;
           etalonnage2Points = 0;
+          //Once the calibration is done we calculate the slope and the offset
           sensi = ((((mesurePoint2 - mesurePoint1)/R0)*100.0)/(courburePoint2 - courburePoint1));
           offset = (mesurePoint1 - (sensi * courburePoint1/R0));
           selection = 1;
@@ -175,6 +182,7 @@ void loop() {
       }
       break;
     case 5:
+      //Menu in which you can choose which menu to go to 
       MenuChoix();
       if(btnState == LOW and btnLastState != LOW){mode = selection; etalonnage2Points = 0; courbure = 0;}
       break;
@@ -183,12 +191,11 @@ void loop() {
       if(btnState == LOW and btnLastState != LOW){mode = 5;}
       break;
   }
-
-  varSelection = 0;
-  
 }
 
-/*Fonction qui donne la valeur du rotoEncodeur*/
+
+//=============================Roto Encoder=========================
+/*Function that detects when the Roto Encoder spins*/
 void doEncoder() {
   if(rotoTimer - rotoT0 > 50 ){
     if (digitalRead(encoderPinB) != digitalRead(encoderPinA) and digitalRead(encoderPinB) != digitalRead(encoderPinA)){
@@ -217,6 +224,8 @@ void doEncoder() {
   }
 }
 
+//=============================OLED Text Drawing====================
+/*Function that draws text on the OLED Screen*/
 void Affichage(String text, int x, int y, int taille, boolean actualiser){
   display.setTextSize(taille);
   display.setTextColor(WHITE);
@@ -227,11 +236,14 @@ void Affichage(String text, int x, int y, int taille, boolean actualiser){
   }
 }
 
+//=============================Measurement Menu=====================
+/*Function that draws the measurement menu every 300 ms*/
 void MenuMesure(){
   if(Timer - T0 > 300){
     Affichage("Tension :", 4, 3, 1, false);
     String text = String(valeur * 1000.0);
     String text1 = String(resistance);
+    if(resistance == -1){text1 = "inf";}    
     String text2 = String(mesure);
     Affichage(text, 60, 3, 1, false);
     Affichage("mV", 100, 3, 1, false);
@@ -240,40 +252,37 @@ void MenuMesure(){
     Affichage("MOhm", 100, 11, 1, false);
     Affichage(text2, 60, 21,1,false);
     Affichage("Courbure:", 4, 21, 1, false);
-    Affichage("mm", 100, 21, 1, false);
-    display.drawRect(1, 1, 126,31, WHITE);
-    display.display();
+    Affichage("mm", 100, 21, 1, true);
     T0 = millis();
   }
   Timer = millis();
 }
 
+//=============================Main Menu============================
+/*Function that draws the main menu*/
 void MenuChoix(){
   Affichage("Menu Mesure", 4, 3, 1, false);
   Affichage("Menu Calibration", 4, 11, 1, false);
   Affichage("Etalon 2 Points", 4, 21, 1, false);
-  
   switch (selection){
     case 1:
-      Affichage("<", 110, 3, 1, false);
+      Affichage("<", 110, 3, 1, true);
       break;
     case 2:
-      Affichage("<", 110, 11, 1, false);
+      Affichage("<", 110, 11, 1, true);
       break;
     case 3:
-      Affichage("<", 110, 21, 1, false);
+      Affichage("<", 110, 21, 1, true);
       break;
     default:
-      Affichage("<", 110, 3, 1, false);
+      Affichage("<", 110, 3, 1, true);
       break;
   }
-  display.drawRect(1, 1, 126,31, WHITE);
-  display.display();
 }
 
-
+//=============================Calibration Menu=====================
+/*Function that draws the calibration menu*/
 void MenuEtalonnage(){
-
   String text1 = String(sensi);
   String text2 = String(offset);
   Affichage("Sensi:", 4, 3, 1, false);
@@ -283,11 +292,11 @@ void MenuEtalonnage(){
   String R0string = String(R0);
   Affichage("R0:", 4, 21, 1, false);
   Affichage(R0string, 72, 21, 1, false);
-  Affichage("MOhm", 100, 21, 1, false);
-  display.drawRect(1, 1, 126,31, WHITE);
-  display.display();
+  Affichage("MOhm", 100, 21, 1, true);
 }
 
+//=============================2 Points Calibration Menu============
+/*Function that draws the 2 points calibration menu*/
 void MenuEtalonnage2Points(){
   String text = String(valeur * 1000.0);
   if(etalonnage2Points == 0){
@@ -299,7 +308,7 @@ void MenuEtalonnage2Points(){
     Affichage("Appuyez", 4, 11, 1, false);
     Affichage("Mesure:", 4, 21, 1, false);
     Affichage(text, 72, 21, 1, false);
-    Affichage("mV", 110, 21, 1, false);
+    Affichage("mV", 110, 21, 1, true);
   }else{
     String text1 = String(courbure);
     Affichage("Courbure :", 4, 3, 1, false);
@@ -307,7 +316,7 @@ void MenuEtalonnage2Points(){
     Affichage("Appuyez", 4, 11, 1, false);
     Affichage("Mesure:", 4, 21, 1, false);
     Affichage(text, 72, 21, 1, false);
-    Affichage("mV", 110, 21, 1, false);  
+    Affichage("mV", 110, 21, 1, true);  
   }
   if(etalonnage2Points == 1){
     mesurePoint1 = resistance;
@@ -317,21 +326,17 @@ void MenuEtalonnage2Points(){
     mesurePoint2 = resistance;
     courburePoint2 = courbure;
   }
-  
-  
-  
-  display.drawRect(1, 1, 126,31, WHITE);
-  display.display();
 }
 
-void Introduction(){
+//=============================Introduction=========================
+/*Function that display our team's name*/
+void Introduction(int i){
   display.clearDisplay();
-  Affichage("  LES ROUTEURS DE", 4, 7, 1, false);
-  Affichage("     L'EXTREME", 4, 15, 1, false);
-  display.fillRect(4, 23, display.width()-8, 8, SSD1306_INVERSE);
-  display.drawRect(1, 1, 126,31, WHITE);
+  Affichage("  LES ROUTEURS DE", i-64, 7, 1, false);
+  Affichage("     L'EXTREME", i-64, 15, 1, false);
+  display.fillRect(i-64, 23, display.width()-8, 8, SSD1306_INVERSE);
   display.setTextColor(BLACK);
-  display.setCursor(4,23);
+  display.setCursor(i-64,23);
   display.setTextSize(0.5);
   display.println("            Bouf INC");
   display.display();
